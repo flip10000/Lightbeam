70d8170396b3cf58030acb6cc7c35f9f5a13cf44
cross <cross@www.scadventure.de>
2011-02-15 20:52:35 +0100

    ADD: Editor-Toolbar hinzugefügt!

---
+++ src/lightbeam/editor/Toolbar.java
@@ -0,0 +1,81 @@
+package lightbeam.editor;
+
+import java.awt.ComponentOrientation;
+import java.awt.Dimension;
+import java.awt.Rectangle;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JToolBar;
+
+public class Toolbar 
+{
+	private Editor editor					= null;
+	private JToolBar toolBar				= null;
+	private JButton btnSave					= null;
+	private JButton btnNew					= null;
+	private JButton btnOpen 				= null;
+	private JButton btnClose				= null;
+
+	public Toolbar( Editor editor ) 
+	{
+		this.editor				= editor;
+        this.toolBar			= new JToolBar( "Toolbar", JToolBar.HORIZONTAL );
+        this.toolBar.setLayout( null );
+        this.toolBar.setPreferredSize( new Dimension( editor.getFrame().getWidth(), 38 ) );
+        this.toolBar.setComponentOrientation( ComponentOrientation.LEFT_TO_RIGHT );
+        
+		// ToolBar füllen
+		ImageIcon imgNew		= new ImageIcon( "src/fx/Toolbar/newMap.png" );
+		ImageIcon imgOpen 		= new ImageIcon( "src/fx/Toolbar/open.png" );
+		ImageIcon imgSave 		= new ImageIcon( "src/fx/Toolbar/save.png" );
+		ImageIcon imgClose 		= new ImageIcon( "src/fx/Toolbar/close.png" );		
+        
+		this.btnNew 			= new JButton( imgNew );
+		this.btnOpen 			= new JButton( imgOpen );
+		this.btnSave 			= new JButton( imgSave );
+		this.btnClose 			= new JButton( imgClose );
+
+		this.btnNew.setBounds( new Rectangle( 16, 3, 32, 32 ) );
+		this.btnOpen.setBounds( new Rectangle( this.btnNew.getBounds().x + this.btnNew.getBounds().width, 3, 32, 32 ) );
+		this.btnSave.setBounds( new Rectangle( this.btnOpen.getBounds().x +this.btnOpen.getBounds().width, 3, 32, 32 ) );
+		
+		this.toolBar.add( this.btnNew );
+		this.toolBar.add( this.btnOpen );
+		this.toolBar.add( this.btnSave );
+		this.toolBar.add( this.btnClose );
+		
+		// Neue map ertellen:
+		this.btnNew.addActionListener(new ActionListener(){
+			public void actionPerformed(ActionEvent e) {
+				Toolbar.this.editor.newMap();
+			}
+		});		
+		
+		// Map laden/öffnen:
+		this.btnOpen.addActionListener(new ActionListener(){
+			public void actionPerformed(ActionEvent e) {
+				Toolbar.this.editor.loadMap();
+			}
+		});
+
+		// Map speichern:
+		this.btnSave.addActionListener(new ActionListener(){
+			public void actionPerformed(ActionEvent e) {
+				Toolbar.this.editor.saveMap();
+			}
+		});
+
+		// Editor schließen:
+		this.btnClose.addActionListener(new ActionListener(){
+			public void actionPerformed(ActionEvent e) {
+				Toolbar.this.editor.closeEditor();
+			}
+		});
+        
+	}
+	
+	public JToolBar get() { return this.toolBar; }
+}
