008f45a6062f418e572cd04904a8130ee787ebdf
cross <cross@www.scadventure.de>
2011-02-17 21:34:31 +0100

    CHG: Vollständig abgeschlossen!

--- src/lightbeam/editor/dialogs/OpenDialog.java
+++ src/lightbeam/editor/dialogs/OpenDialog.java
@@ -1,27 +1,27 @@
 package lightbeam.editor.dialogs;
 
-import java.awt.BorderLayout;
 import java.awt.Cursor;
 import java.awt.Dimension;
 import java.awt.Rectangle;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 import java.io.BufferedInputStream;
-import java.io.BufferedOutputStream;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.ObjectInputStream;
-import java.io.ObjectOutputStream;
+import java.util.ArrayList;
 
 import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
+import javax.swing.JTextField;
 import javax.swing.ListSelectionModel;
 import javax.swing.table.DefaultTableModel;
-import lightbeam.editor.MapArea;
+import core.tilestate.TileArray;
 
 public class OpenDialog
 {
@@ -32,7 +32,9 @@ public class OpenDialog
 	private SettingsDialog dSettings	= new SettingsDialog();
 	private JPanel panel				= new JPanel();
 	private JLabel lblOpen				= new JLabel( "Kartenauswahl:" );
+	private JLabel lblMapSelection		= new JLabel( "Aktuelle Auswahl:" );
 	private String[][] mapRows			= null;
+	private String[] mapDest			= null;
 	private DefaultTableModel mapModel 	= new DefaultTableModel( mapRows, mapCols );
 	private JTable mapTable				= new JTable( mapModel ) 
 	{
@@ -42,7 +44,13 @@ public class OpenDialog
     };
     
 	private JScrollPane scroll			= new JScrollPane( mapTable );
-	private MapArea mapArea				= null;
+	private JTextField txtMapSelected	= new JTextField( "<keine Auswahl>" );
+	private String selMapDest			= null;
+	
+	private String loadedMapName		= null;
+	private TileArray loadedTileArray	= null;
+	private String loadedDifficulty		= null;
+	private String loadedBuildStatus	= null;
 	
 	public OpenDialog()	
 	{
@@ -56,10 +64,22 @@ public class OpenDialog
 		this.mapTable.setFillsViewportHeight( true );
 		this.mapTable.setSelectionMode( ListSelectionModel.SINGLE_SELECTION );
 		this.mapTable.setCursor( new Cursor( Cursor.HAND_CURSOR ) );
-//		this.mapTable.
+		this.mapTable.addMouseListener(new MouseAdapter()
+		{
+			public void mouseClicked( MouseEvent e )
+			{
+				OpenDialog.this.setRowFocus();
+			}
+		});
+
+		this.lblMapSelection.setBounds( new Rectangle( this.scroll.getBounds().x, this.scroll.getBounds().y + this.scroll.getBounds().height, 110, 20 ) );
+		this.txtMapSelected.setBounds( this.lblMapSelection.getBounds().x + this.lblMapSelection.getBounds().width, this.scroll.getBounds().y + this.scroll.getBounds().height, this.scroll.getBounds().width - this.lblMapSelection.getBounds().width, 20 );
+		this.txtMapSelected.setEditable( false );
 		
 		this.panel.add( this.lblOpen );
 		this.panel.add( this.scroll );
+		this.panel.add( this.lblMapSelection );
+		this.panel.add( this.txtMapSelected );
 	}
 	
 	public void showDialog()
@@ -70,168 +90,67 @@ public class OpenDialog
 		this.pane 		= new JOptionPane( this.panel, JOptionPane.PLAIN_MESSAGE, JOptionPane.OK_CANCEL_OPTION );		
 		this.pane.createDialog( "Karte öffnen" ).setVisible( true );
 
-//		if( this.pane.getValue() != null )
-//		{
-//			int selOption	= ( (Integer)this.pane.getValue() ).intValue();
-//			
-//			if( selOption == JOptionPane.OK_OPTION )
-//			{
-//				String mapName			= inpMap.getText();
-//				SettingsDialog dialog	= new SettingsDialog();
-//				String pathMaps			= dialog.getPath();
-//				
-//				if( !pathMaps.equals( "" ) && !mapName.equals( "" ) )
-//				{
-//					this.proceedSaving( pathMaps, mapName );
-//				} else
-//				{
-//					if( pathMaps.equals( "" ) )
-//					{
-//						JPanel panelNoPath	= new JPanel();
-//						
-//						panelNoPath.setLayout( new BorderLayout() );
-//						
-//						JLabel lblHint		= new JLabel( "Zur Speicherung Ihrer Karte müssen Sie einen Zielpfad festlegen!" );
-//						JLabel lblHint2		= new JLabel( "Klicken Sie auf \"Ok\" um diesen festzulegen!" );
-//						
-//						panelNoPath.add( lblHint, BorderLayout.NORTH );
-//						panelNoPath.add( lblHint2, BorderLayout.SOUTH );
-//						
-//						JOptionPane dSetPath	= new JOptionPane( panelNoPath, JOptionPane.INFORMATION_MESSAGE, JOptionPane.OK_CANCEL_OPTION );
-//						dSetPath.createDialog( "Speicherort hinterlegen!" ).setVisible( true );
-//						
-//						if( dSetPath.getValue() != null )
-//						{
-//							int selected	= ( (Integer)dSetPath.getValue() ).intValue();
-//							
-//							if( selected == 0 )
-//							{
-//								dialog.showDialog();
-//								this.showDialog();
-//							}
-//						}
-//					}
-//				}
-//			}
-//		}
+		if( this.pane.getValue() != null )
+		{
+			int selOption	= ( (Integer)this.pane.getValue() ).intValue();
+			
+			if( selOption == JOptionPane.OK_OPTION && this.selMapDest != null )
+			{
+				this.loadMap();
+			}
+		}
+	}
+
+	public ArrayList<Object> getMap()
+	{
+		ArrayList<Object> retVal	= new ArrayList<Object>();
+		
+		retVal.add( this.loadedMapName );
+		retVal.add( this.loadedTileArray );
+		retVal.add( this.loadedDifficulty );
+		retVal.add( this.loadedBuildStatus );
+		
+		return retVal;
 	}
 	
-	private String getMapName( String mapDest )
+	private void loadMap()
 	{
-		// ToDo: Prüfen, ob typ in mapDest == .map!
+		FileInputStream file;
+		
 		try 
 		{
-			FileInputStream file	= new FileInputStream( mapDest );
+			file 					= new FileInputStream( this.selMapDest );
 			BufferedInputStream buf	= new BufferedInputStream( file );
-	
-			try 
+			ObjectInputStream read;
+			
+			try
 			{
-				ObjectInputStream read	= new ObjectInputStream( buf );
+				read 				= new ObjectInputStream( buf );
+				
 				try 
 				{
-					String fMapName		= (String) read.readObject();
-					
-					read.close();
-					
-					return fMapName;
-				} catch (ClassNotFoundException e) 
+					// Geladener Map-Name:
+					this.loadedMapName		= (String) read.readObject();
+					// Geladenes Map-TileArray:
+					this.loadedTileArray	= (TileArray) read.readObject();
+					// ToDo: Geladener Map-Schwierigkeitsgrad:					
+//					this.loadedDifficulty	= (String) read.readObject();
+					this.loadedDifficulty	= "Leicht";
+					// ToDo: Geladener Map-Status:
+//					this.loadedBuildStatus	= (String) read.readObject();
+					this.loadedBuildStatus	= "Spielbar";
+				} catch( ClassNotFoundException e )
 				{
-					// TODO Fehler ausgeben, das map nicht gepsiechert werden kann,
-					// da Zugriff auf vorhandene Dateien nichtm öglich ist, um zu überprüfen, ob
-					// map bereits vorhanden!
+					// TODO Passende Fehlermeldung (read.close() muss bleiben!!!)!
 					read.close();
-					
-					return null;
 				}
 			} catch( IOException e )
 			{
-				return null;
+				// TODO Passende Fehlermeldung
 			}
 		} catch( FileNotFoundException e ) 
 		{
-			return null;
-		}		
-	}
-	
-	private void proceedSaving( String pathMaps, String mapName )
-	{
-		File dir		= new File( pathMaps );
-		String[] files	= dir.list();
-		int amount		= 0;
-		boolean mExists	= false;
-		
-		if( files != null )
-		{
-			amount		= files.length;
-			
-			for( int count = 0; count < amount; count++ )
-			{
-				String fileDest			= pathMaps + "/" + files[count];
-				String fMapName			= this.getMapName( fileDest );
-				
-				if( fMapName != null && fMapName.equals( mapName ) )
-				{
-					mExists	= true;
-					break;
-				}
-			}
-		}
-		
-		if( !mExists )
-		{
-			this.saveMap( pathMaps + "/map" + amount + this.extMap, mapName );
-		} else
-		{
-			JPanel panelQuestion	= new JPanel();
-			
-			panelQuestion.setLayout( new BorderLayout() );
-			
-			JLabel lblQuestion		= new JLabel( "Eine Karte mit dem Kartennamen \""+mapName+"\" ist bereits vorhanden!" );
-			JLabel lblQuestion2		= new JLabel( "Soll die bestehende Karte überschrieben werden?" );
-			
-			panelQuestion.add( lblQuestion, BorderLayout.NORTH );
-			panelQuestion.add( lblQuestion2, BorderLayout.SOUTH );
-			
-			JOptionPane dOverwrite	= new JOptionPane( panelQuestion, JOptionPane.QUESTION_MESSAGE, JOptionPane.YES_NO_OPTION );
-			dOverwrite.createDialog( "Frage!" ).setVisible( true );
-			
-			if( dOverwrite.getValue() != null )
-			{
-				int selected	= ( (Integer)dOverwrite.getValue() ).intValue();
-
-				if( selected == 0 )
-				{
-					this.saveMap( pathMaps + "/map" + amount + this.extMap, mapName );
-				} else
-				{
-					this.showDialog();
-				}
-			}
-		}		
-	}
-	
-	private void saveMap( String mapDest, String mapName )
-	{
-		try 
-		{
-			FileOutputStream file		= new FileOutputStream( mapDest );
-			BufferedOutputStream buf	= new BufferedOutputStream( file );
-			
-			try 
-			{
-				ObjectOutputStream write	= new ObjectOutputStream( buf );
-				
-				write.writeObject( mapName );
-				write.writeObject( this.mapArea.getMap() );
-
-				write.close();
-			} catch( IOException e )
-			{
-				// TODO Auto-generated catch block
-			}
-		} catch( FileNotFoundException e )
-		{
-			// TODO Auto-generated catch block
+			// TODO Passende Fehlermeldung
 		}
 	}
 	
@@ -259,7 +178,7 @@ public class OpenDialog
 					
 					if( strLen == strPos )
 					{
-						String fDest			= pathMaps + "/" + files[count];
+						String fDest	= pathMaps + "/" + files[count];
 						
 						try 
 						{
@@ -274,10 +193,11 @@ public class OpenDialog
 								{
 									String 	mapName			= (String) read.readObject();
 									
-									rows[cntMap]			= new String[3];
+									rows[cntMap]			= new String[4];
 									rows[cntMap][0]			= mapName;
 									rows[cntMap][1]			= "kA";
 									rows[cntMap][2]			= "kA";
+									rows[cntMap][3]			= fDest;
 									
 									cntMap++;
 									
@@ -300,6 +220,7 @@ public class OpenDialog
 		if( rows != null )
 		{
 			this.mapRows		= new String[cntMap][];
+			this.mapDest		= new String[cntMap];
 			
 			for( int cntRow	= 0; cntRow < cntMap; cntRow++ )
 			{
@@ -307,15 +228,18 @@ public class OpenDialog
 				this.mapRows[cntRow][0]	= rows[cntRow][0];
 				this.mapRows[cntRow][1]	= rows[cntRow][1];
 				this.mapRows[cntRow][2]	= rows[cntRow][2];
+				this.mapDest[cntRow]	= rows[cntRow][3];
 			}
 		} else
 		{
+			this.mapDest		= new String[1];
 			this.mapRows		= new String[1][];
 			this.mapRows[0]		= new String[3];
 			
-			this.mapRows[0][0]	= "a"; 
-			this.mapRows[0][1]	= "a";
-			this.mapRows[0][2]	= "a";
+			this.mapRows[0][0]	= ""; 
+			this.mapRows[0][1]	= "";
+			this.mapRows[0][2]	= "";
+			this.mapDest[0]		= "";			
 		}
 		
 		for( int i=0; i< this.mapRows.length; i++ )
@@ -326,6 +250,22 @@ public class OpenDialog
 	
 	private void resetTable()
 	{
+		this.mapDest		= null;
+		this.mapRows		= null;
+		this.selMapDest		= null;
+		this.txtMapSelected.setText( "<keine Auswahl>" );
+		
 		this.mapTable.removeAll();		
+	}
+	
+	private void setRowFocus()
+	{
+		int rowSelected = this.mapTable.getSelectedRow();
+		
+		if( rowSelected > -1 )
+		{
+			this.selMapDest		= this.mapDest[rowSelected];
+			this.txtMapSelected.setText( (String)this.mapTable.getModel().getValueAt( rowSelected, 0 ) );
+		}
 	}
 }
